0b1896ce38a0dc5faa142f55063571f2b7d3e5d3
itu-itis21-taflan21 <92297097+itu-itis21-taflan21@users.noreply.github.com>
2025-01-03 22:12:36 +0300

    author search

--- BookVerseFrontend/vite.config.js
+++ BookVerseFrontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://54.160.153.61:8000',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
